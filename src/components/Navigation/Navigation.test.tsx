c907ffe5f6b37b04aee0c0b411a8a46eb959b093
jagodabie <51316252+jagodabie@users.noreply.github.com>
2024-01-03 06:43:42 +0100

    test(): navigation unit test

--- src/components/Navigation/Navigation.test.tsx
+++ src/components/Navigation/Navigation.test.tsx
@@ -2,30 +2,122 @@ import { screen } from '@testing-library/react';
 
 import { ThemeProvider } from 'styled-components';
 
-import { whiteTheme } from '../../const';
+import { Switch } from '@mui/material';
 import { render } from '../../test-utils';
-import { StyledList, StyledNav } from '.';
+import { whiteTheme } from '../../const';
+import Navigation from './Navigation';
 
 describe('Navigation', () => {
+  test('pass toggle button to navigation and renders correctly', () => {
+    render(
+      <ThemeProvider theme={whiteTheme}>
+        <Navigation
+          toggleButton={
+            <Switch
+              inputProps={{
+                'aria-label': 'enable dark mode test',
+              }}
+            />
+          }
+        />
+      </ThemeProvider>
+    );
+
+    const switchElement = screen.getByRole('checkbox', {
+      name: 'enable dark mode test',
+    });
+    expect(switchElement).toBeInTheDocument();
+  });
+  test('renders button hamburger correctly', () => {
+    render(
+      <ThemeProvider theme={whiteTheme}>
+        <Navigation
+          toggleButton={
+            <Switch
+              inputProps={{
+                'aria-label': 'enable dark mode test',
+              }}
+            />
+          }
+        />
+      </ThemeProvider>
+    );
+    const hamburgerButton = screen.queryByRole('button', {
+      name: 'hamburger',
+    });
+    expect(hamburgerButton).toBeInTheDocument();
+  });
+
+  test('when hamburger is clicked, menu list is opened', () => {
+    render(
+      <ThemeProvider theme={whiteTheme}>
+        <Navigation
+          toggleButton={
+            <Switch
+              inputProps={{
+                'aria-label': 'enable dark mode test',
+              }}
+            />
+          }
+        />
+      </ThemeProvider>
+    );
+    const hamburgerButton = screen.queryByRole('button', {
+      name: 'hamburger',
+    });
+    
+    expect(hamburgerButton).toBeInTheDocument();
+  });
   test('renders nav tag correctly', () => {
     render(
       <ThemeProvider theme={whiteTheme}>
-        <StyledNav />
+        <Navigation
+          toggleButton={
+            <Switch
+              inputProps={{
+                'aria-label': 'enable dark mode test',
+              }}
+            />
+          }
+        />
       </ThemeProvider>
     );
     const navigationElement = screen.getByRole('navigation');
     expect(navigationElement).toBeInTheDocument();
   });
+
   test('renders ul tag correctly', () => {
     render(
       <ThemeProvider theme={whiteTheme}>
-        <StyledNav>
-          <StyledList open={false} />
-        </StyledNav>
+        <Navigation
+          toggleButton={
+            <Switch
+              inputProps={{
+                'aria-label': 'enable dark mode test',
+              }}
+            />
+          }
+        />
       </ThemeProvider>
     );
     const listElement = screen.getByRole('list');
     expect(listElement).toBeInTheDocument();
   });
-  test('renders li tags correctly', () => {});
+  test('renders li tag correctly', () => {
+    render(
+      <ThemeProvider theme={whiteTheme}>
+        <Navigation
+          toggleButton={
+            <Switch
+              inputProps={{
+                'aria-label': 'enable dark mode test',
+              }}
+            />
+          }
+        />
+      </ThemeProvider>
+    );
+    const listElement = screen.getByRole('listitem');
+    expect(listElement).toBeInTheDocument();
+  });
 });
