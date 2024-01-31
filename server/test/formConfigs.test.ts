a2c20051442571e92da5f250f840931ff456261a
jagodabie <51316252+jagodabie@users.noreply.github.com>
2024-01-31 11:00:08 +0100

    test(BE): formConfig route unit test

---
+++ server/test/formConfigs.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import app from '../src/app';
+import { FormConfigs } from '../src/api/formConfig/formConfig.model';
+
+beforeAll(async () => {
+  try {
+    FormConfigs.drop();
+  } catch (error) {}
+});
+
+describe('GET /api/v1/form-configs', () => {
+  it('responds with a json message', async () => {
+    await request(app)
+      .get('/api/v1/form-configs') 
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveProperty('length');
+        expect(response.body).toEqual([]);
+      });
+  }); 
+});
